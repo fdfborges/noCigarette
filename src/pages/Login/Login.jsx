022847ebce2273a76c774c98096398fdbc5e5bb4
FelipeIE <felipecorpiefe@Gmail.com>
2025-07-24 16:08:58 -0300

    teste

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,6 +3,8 @@ import './Login.scss';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
+import logonoCigarrete from '../../assets/logonoCigarrete.png'
+import FelipeAvatar from '../../assets/FelipeAvatar.png';
 
 export const Login = () => {
 
@@ -14,22 +16,32 @@ export const Login = () => {
 
   return (
     <>
-      <div className="containerAuthGoogle">
-        <GoogleLogin
-          type='standard'
-          size='large'
-          theme='outline'
-          text='continue_with'
-          shape='square'
-          logo_alignment='left'
-          width={""}
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse)
-            console.log(jwtDecode(credentialResponse.credential));
-            navigate("/Sondagem");
-          }}
-          onError={() => console.log("Login Fail")}
-        />
+      <div className="containerLogin_RegisterPrincipal">
+        <div className="ContainerLoginCima">
+          <img src={logonoCigarrete} alt="Logo noCigarette" />
+          <GoogleLogin className="btnGoogle"
+            type='standard'
+            size='large'
+            theme='outline'
+            text='continue_with'
+            shape='square'
+            logo_alignment='left'
+            width={""}
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse)
+              console.log(jwtDecode(credentialResponse.credential));
+              navigate("/Sondagem");
+            }}
+            onError={() => console.log("Login Fail")} />
+        </div>
+        <div className="ContainerLoginBaixo">
+            <div className="ContainerLoginBaixoDireita">
+              <h2>Diga não ao cigarro</h2>
+              <div className="MsgEntreGoogle"><p>Entre com o Google</p></div>
+              </div>
+            <div className="ContainerLoginBaixoEsquerda"><img src={FelipeAvatar} alt="" /></div>
+        </div>
+
       </div>
     </>
   )
