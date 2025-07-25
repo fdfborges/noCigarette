63d1043921165adf923df807173bb518522f7c3d
FelipeIE <felipecorpiefe@Gmail.com>
2025-07-25 12:31:41 -0300

    resolving bugs vercel

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,8 +3,6 @@ import './Login.scss';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
-import logonoCigarrete from '../../assets/logonoCigarrete.png'
-import FelipeAvatar from '../../assets/FelipeAvatar.png';
 
 export const Login = () => {
 
