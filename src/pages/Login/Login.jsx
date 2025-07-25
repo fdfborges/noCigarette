f1cc8e86198f5486b23d385f71b9676c2513a69e
FelipeIE <felipecorpiefe@Gmail.com>
2025-07-25 18:40:29 -0300

    Criada a pagina de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Login.scss';
 import Logo from '../../components/logo/Logo';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import FormLogin from '../../components/FormLogin/FormLogin';
@@ -11,11 +12,32 @@ export default function Login() {
         <div className="containerLoginPrincipal">
             <div className="containerHeaderLogin">
                 <Logo />
+                <p className='LoginSubtitle'>A no noCigarrete é a aplicação que te fará parar com seu vicio, ter uma vida mais saudável, mais anos de vida. parar com seu vicio, ter uma vida mais saudável, mais anos de vida.</p>
             </div>
             <div className="containerMeioLogin">
                 <FormLogin />
             </div>
-            <div className="containerFooterLogin"></div>
+            <div className="containerFooterLogin">
+                <div className="FooterLineOr">
+                    <hr />
+                    <span>or</span>
+                    <hr />
+                </div>
+                <GoogleLogin className="btnGoogle"
+                    type='standard'
+                    size='large'
+                    theme='filled_black'
+                    text='sign_in_with'
+                    shape='square'
+                    logo_alignment='left'
+                    width={""}
+                    onSuccess={(credentialResponse) => {
+                        console.log(credentialResponse)
+                        console.log(jwtDecode(credentialResponse.credential));
+                        navigate("/Sondagem");
+                    }}
+                    onError={() => console.log("Login Fail")} />
+            </div>
         </div>
     )
 }
