7b31ae8a9d447b5a726ae7e1b822faba2d408ed7
FelipeIE <felipecorpiefe@Gmail.com>
2025-07-31 12:17:56 -0300

    created page 4 of the survey

--- src/components/FormStep/Quest4.jsx
+++ src/components/FormStep/Quest4.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ButtonPlusMinus from '../ButtonPlusMinus/ButtonPlusMinus'
 
 export default function Quest4() {
   return (
-    <div>
-        <h2>Quest4</h2>
+    <div className='containerPrincipalQuest4'>
+      <h1>Quantos cigarros vêm no maço que você geralmente compra?</h1>
+      <ButtonPlusMinus />
     </div>
   )
 }
